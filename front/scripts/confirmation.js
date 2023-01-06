b4b5bca26844bd1a3abf3796fff54bb86e08a14b
PHILIPPINE HEDIN <philippine.hedin@hotmail.fr>
2023-01-06 17:28:08 +0100

    Display orderId in confirmation.html

---
+++ front/scripts/confirmation.js
@@ -0,0 +1,7 @@
+const orderId = new URLSearchParams(location.search).get('orderId');
+
+document.getElementById('orderId').innerHTML = orderId;
+
+const thanks = document.createElement('p');
+thanks.innerText = "Merci de votre achat !";
+document.querySelector('.confirmation p').appendChild(thanks);
